75f0b7147e1984086e2addbaecf11233bb7e7da5
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-11 17:49:36 +0530

    home_page view deleted

--- saas/src/cfehome/views.py
+++ saas/src/cfehome/views.py
@@ -8,33 +8,23 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
-	#returns everything
-	qs = PageVisit.objects.all()
-	page_qs = PageVisit.objects.filter(path=request.path)
-	my_title = "My page"
-	my_context = {
-		"page_title": my_title,
-		"page_visit_count": page_qs.count(),
-		"total_page_visit_count": qs.count(),
-		"percent": (page_qs.count() * 100.0) / qs.count()
-	}
-	path = request.path
-	print('path: ', path)
-	PageVisit.objects.create(path=request.path)
-	html_template = "home.html"
-	return render(request, html_template, my_context)
+	return about_view(request, *args, **kwargs)
 
 
 def about_view(request, *args, **kwargs):
 	qs = PageVisit.objects.all()
 	page_qs = PageVisit.objects.filter(path=request.path)
+	try:
+		percent = (page_qs.count() * 100.0) / qs.count()
+	except:
+		percent = 0
 	my_title = "My Page"
 	html_template = "home.html"
 	my_context = {
 		"page_title": my_title,
 		"page_visit_count": page_qs.count(),
 		"total_page_visit_count": qs.count(),
-		"percent": (page_qs.count() * 100.0) / qs.count()
+		"percent": percent
 	}
 	PageVisit.objects.create(path=request.path)
 	return render(request, html_template, my_context)
