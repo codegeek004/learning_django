f968a364197438a0ab7f6491b3f3e74c8fd08fd1
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-15 13:11:38 +0530

    Added django environ var

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ print(BASE_DIR, 'BASE_DIR')
 SECRET_KEY = 'django-insecure-r(#=&qjp8u9)&tsggngl9__-0s675zfac&ud*_ry2_o4)g9gr0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true" 
+print("DEBUG", type(DEBUG))
 
 ALLOWED_HOSTS = [
     '.railway.app' # https://saas.prod.railway.app
