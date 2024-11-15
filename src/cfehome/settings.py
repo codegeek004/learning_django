80d0261ad5b01d391a1f4278c55978458ec1646c
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-15 23:49:45 +0530

    Neon postgresql database added

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -91,6 +91,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASE_URL = config("DATABASE_URL", default=None, cast=str)
+CONN_MAX_AGE = config("CONN_MAX_AGE", default=30, cast=int)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=30,
+        conn_health_checks=True,
+        )
+}
 
 
 # Password validation
