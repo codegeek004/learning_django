c19625e270ba68949fdc66d2ef3a902b4a12eeae
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-19 10:20:36 +0530

    Added configuration for load static

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -140,6 +140,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source for python manage.py collectstatic
+STATICFILES_DIR = [
+#     STATICFILES_BASE_DIR,
+#     STATICFILES_VENDOR_DIR, 
+    BASE_DIR / "staticfiles",        # Your main static files directory
+    # BASE_DIR / "staticfiles/vendors" 
+]
+
+#output for python manage.py collectstatic
+#local cdn
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "prod-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
