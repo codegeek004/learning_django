df1d135882fa4d84c6250c6ef836465ebdfa8461
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-20 11:34:35 +0530

    storing cdn on local

---
+++ src/commando/management/commands/vendor_pull.py
@@ -0,0 +1,34 @@
+import helpers
+from typing import Any
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+VENDOR_STATICFILES = {
+	 "flowbite.min.css": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
+    "flowbite.min.js": "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"	
+}
+
+STATICFILES_VENDOR_DIR = getattr(settings, 'STATICFILES_VENDOR_DIR')
+
+class Command(BaseCommand):
+	def handle(self, *args: Any, **options: Any):
+		self.stdout.write("Downloading vendor static files")
+		completed_urls = []
+		for name,url in VENDOR_STATICFILES.items():
+			out_path = STATICFILES_VENDOR_DIR / name
+			dl_success = helpers.download_to_local(url, out_path)
+			print(name, url, out_path)
+			if dl_success:
+				completed_urls.append(url)
+			else:
+				self.stdout.write(
+					self.style.ERROR(f"Failed to download {url}")
+					)
+		if set(completed_urls) == set(VENDOR_STATICFILES.values()):
+			self.stdout.write(
+				self.style.SUCCESS('Successfully updated vendor static files')
+				)
+		else:
+			self.stdout.write(
+				self.style.WARNING('Some files were not updated')
+				)
