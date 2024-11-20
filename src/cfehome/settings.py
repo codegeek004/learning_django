0d61d52de6d619d463fed2eb3964638ce434385a
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-20 11:35:50 +0530

    Storing cdn on local

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR, 'BASE_DIR')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -25,7 +24,6 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 #configuring the hard way
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true" 
 DEBUG = config("DJANGO_DEBUG")
-print("DEBUG", DEBUG)
 
 ALLOWED_HOSTS = [
     '.railway.app' # https://saas.prod.railway.app
@@ -48,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #my apps
     'visits',
+    'commando',
 ]
 
 MIDDLEWARE = [
@@ -140,8 +139,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
-# STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+# STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+print(STATICFILES_VENDOR_DIR, 'vendor dir')
 
 # source for python manage.py collectstatic
 STATICFILES_DIRS = [
