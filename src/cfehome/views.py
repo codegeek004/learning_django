c43c1aa5a39caf275e6f09d4d2032ecf05bf4914
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-22 10:10:26 +0530

    login_required and staff_member_only decorators

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -2,8 +2,14 @@ import pathlib
 from pathlib import Path
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from visits.models import PageVisit
+from django.conf import settings
+
+
+#if no admin is found for the staff required
+LOGIN_URL = settings.LOGIN_URL
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
@@ -48,4 +54,36 @@ def my_old_home_page(request, *args, **kwargs):
 		# html_file_path = this_dir / "home.html"
 		# html_ = html_file_path.read_text()
 	return HttpResponse(html_)
+
+VALID_CODE = 'abc123'
+def pw_protected_view(request, *args, **kwargs):
+	is_allowed = request.session.get('protected_page_allowed') or 0
+	print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed') ))
+	if request.method == 'POST':
+		user_pw_sent = request.POST.get('code') or None
+		print(user_pw_sent, 'user pw sent')	 
+		if user_pw_sent == VALID_CODE:
+			request.session['protected_page_allowed'] = 1
+	
+	if is_allowed:
+		return render(request, 'protected/view.html')
+	return render(request, "protected/entry.html")
+
+
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+	return render(request, 'protected/user_only_view.html')
+
+
+
+
+@staff_member_required(login_url=LOGIN_URL)
+def staff_only_view(request, *args, **kwargs):
+	return render(request, 'protected/staff_only_view.html')
+
+
+
+
+
 
