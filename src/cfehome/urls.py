f55357431d7fac5e58163479ae4685516f8b4d5d
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-22 10:11:08 +0530

    path for login_required and staff_member_only decorators

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_page_view, about_view
+from .views import (
+    home_page_view, 
+    about_view, 
+    pw_protected_view, 
+    user_only_view,
+    staff_only_view
+    )
 from auth import views as auth_views
 
 urlpatterns = [
@@ -29,4 +35,7 @@ urlpatterns = [
     path('hello-world.html', home_page_view), #this is also valid
     path('', home_page_view, name="home"),
     path('accounts/', include('allauth.urls')),
+    path('protected/', pw_protected_view),
+    path('protected/user_only', user_only_view), 
+    path('protected/staff_only', staff_only_view),
 ]
