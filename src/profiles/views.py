75999355ab2a9094008ef67823e617cc133ec012
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-23 17:52:13 +0530

    user permissions

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -14,22 +14,18 @@ def profile_list_view(request):
 
 # Create your views here.
 @login_required
-def profile_view(request, username=None, *args, **kwargs):
+def profile_detail_view(request, username=None, *args, **kwargs):
 	user = request.user 
-
-	print("user.has_perm('auth.view_user')", user.has_perm('auth.view_user'))
-	print("user.has_perm('visits.view_pagevisit')", user.has_perm('visits.view_pagevisit'))	
-	# <app_label>.view_<model_name>
-	# <app_label>.add_<model_name>
-	# <app_label>.change_<model_name>
-	# <app_label>.delete_<model_name>
-
-	# profile_user_obj = User.objects.get('username')
+	user_groups = user.groups.all()
+	print(user_groups, 'user_groups')
+	if user_groups.filter(name__icontains="basic").exists():
+		return HttpResponse("Congrats")
 	profile_user_obj = get_object_or_404(User, username=username)
 	is_me = profile_user_obj == user
-	if is_me:
-		if (user.has_perm('visits.view_pagevisit')):
-			pass
-			# qs = PageVisit.objects.all()
-
-	return HttpResponse(f"Hello there! {username} - {profile_user_obj.id} - {user.id} ")
+	context = {
+		"object" : User.objects.filter(is_active=True),
+		"instance": profile_user_obj,
+		"owner": is_me,
+	}
+	
+	return render(request, 'profiles/detail.html', context)
