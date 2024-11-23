6df772fdba8ebab2ea063fa48af64798a7a6ba79
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-23 13:05:57 +0530

    list_view() for authorization

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -4,14 +4,32 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 User = get_user_model()
 
+
+@login_required
+def profile_list_view(request):
+	context = {
+		"object_list" : User.objects.filter(is_active=True)
+	}
+	return render(request, 'profiles/list.html', context)
+
 # Create your views here.
 @login_required
 def profile_view(request, username=None, *args, **kwargs):
 	user = request.user 
+
+	print("user.has_perm('auth.view_user')", user.has_perm('auth.view_user'))
+	print("user.has_perm('visits.view_pagevisit')", user.has_perm('visits.view_pagevisit'))	
 	# <app_label>.view_<model_name>
 	# <app_label>.add_<model_name>
 	# <app_label>.change_<model_name>
 	# <app_label>.delete_<model_name>
-	print(user.has_perm("auth.view_user"))
+
+	# profile_user_obj = User.objects.get('username')
 	profile_user_obj = get_object_or_404(User, username=username)
+	is_me = profile_user_obj == user
+	if is_me:
+		if (user.has_perm('visits.view_pagevisit')):
+			pass
+			# qs = PageVisit.objects.all()
+
 	return HttpResponse(f"Hello there! {username} - {profile_user_obj.id} - {user.id} ")
