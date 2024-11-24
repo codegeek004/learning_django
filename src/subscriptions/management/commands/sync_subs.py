2a6f2caae24f86560c2c58492064329b4eb7de79
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-24 22:57:05 +0530

    sync group with permissions

--- src/subscriptions/management/commands/sync_subs.py
+++ src/subscriptions/management/commands/sync_subs.py
@@ -5,10 +5,15 @@ from subscriptions.models import Subscription
 class Command(BaseCommand):
 	def handle(self, *args: any, **options: any):
 		# print("Hello World")
-		qs = Subscription.objects.all()
+		qs = Subscription.objects.filter(active=True)
 		for obj in qs:
-			print(obj.groups.all())
+			# print(obj.groups.all())
+			sub_perms = obj.permissions.all() 
 			for group in obj.groups.all():
+				print('group wale for loop mai')
+				print(group)
 				for per in obj.permissions.all():
-					group.permissions.add(per)
+					print('per wale for loop mai')
+					print(per)
+					group.permissions.set(sub_perms)
 			# print(obj.permissions.all())
