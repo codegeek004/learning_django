5350c3ee9a5e1db691ea1682fa86f2a52921fb60
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-25 19:11:35 +0530

    added models for the admin

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Subscription, UserSubscription
+from .models import Subscription, UserSubscription, SubscriptionPrice
 
-admin.site.register(Subscription)
+class SubscriptionPrice(admin.TabularInline):
+	model = SubscriptionPrice
+	extra = 1
+
+class SubscriptionAdmin(admin.ModelAdmin):
+	list_display = ['name', 'active', 'stripe_id']
+
+admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(UserSubscription)
