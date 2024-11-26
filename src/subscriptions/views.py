2a2c6f5da07170aaf0becb190e76b78411da60a9
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-11-26 20:02:35 +0530

    pricing view

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from subscriptions.models import SubscriptionPrice
 
 # Create your views here.
+
+def subscription_price_view(request):
+	qs = SubscriptionPrice.objects.filter(featured=True)
+	monthly_qs = SubscriptionPrice.objects.filter(interval=SubscriptionPrice.IntervalChoices.MONTHLY)
+	# yearly_qs = SubscriptionPrice.objects.filter(interval=SubscriptionPrice.IntervalChoices.YEAR)
+	return render(request, 'subscriptions/pricing.html',{
+		"monthly_qs" : monthly_qs,
+		# "yearly_qs" : yearly_qs
+		})
